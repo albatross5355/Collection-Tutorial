0c32a648dbd5c0889f70657086757dc8247d66cd
albatross5355 <pkddsdubey535@gmail.com>
2024-10-27 18:52:03 +0530

    Added iterator methods

--- src/main/java/com/example/HomeLoanApp/collectiontutorial/NameList.java
+++ src/main/java/com/example/HomeLoanApp/collectiontutorial/NameList.java
@@ -1,6 +1,7 @@
 package com.example.HomeLoanApp.collectiontutorial;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 public class NameList {
 
@@ -12,7 +13,7 @@ public class NameList {
                 names.size(), names);
         // Add some names to the list
         names.add("Ken");
-        names.add("Lee");
+        names.add("Le");
         names.add("Joe");
         // Print the list details
         System.out.printf("After adding 3 elements: Size = %d, Elements = %s%n",
@@ -30,6 +31,35 @@ public class NameList {
         // Print the list details
         System.out.printf("After clearing all elements: Size = %d, Elements = %s%n",
                 names.size(), names);
+
+        // Iteration are of three types
+        //1. Using iterator
+        Iterator<String> iterator = names.iterator();
+        while(iterator.hasNext()){
+            String name = iterator.next();
+            System.out.println("Name:"+name);
+        }
+
+        Iterator<String> iterator1 = names.iterator();
+        while(iterator1.hasNext()){
+            String name = iterator1.next();
+            if (name.length() == 2) {
+                System.out.println("Remove method:"+name);
+                iterator1.remove();
+            }
+        }
+
+        Iterator<String> iterator2 = names.iterator();
+        iterator2.forEachRemaining(System.out::println);
+
+        //Using for each loop
+        for(String name: names){
+            System.out.println(name);
+        }
+
+
+        //using foreach method of iterable interface
+        names.forEach((System.out::println));
 
     }
 }
